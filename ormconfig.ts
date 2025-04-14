7e42eb6d0fcc195917db3b0c0c84279b4770da96
ThanhDong <dongnt22.ite@vku.udn.vn>
2025-04-14 17:35:59 +0700

    Add Docker support with Dockerfile and .dockerignore; update database connection settings in ormconfig.ts; simplify package.json scripts and remove runTunnel.ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,15 @@
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "dong",
-    database: "brainboost",
-    synchronize: false,
-    logging: true,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"],
-    migrationsTableName: "migrations",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "dong",
+  database: "brainboost",
+  synchronize: false,
+  logging: true,
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+  migrationsTableName: "migrations",
 });
