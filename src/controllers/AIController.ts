86f69d07675f54d3fe5dbc7b517e86f35b626c45
ThanhDong <dongnt22.ite@vku.udn.vn>
2025-05-02 15:42:18 +0700

    feat(cache): implement Redis caching for distractor generation and add Redis client configuration

--- src/controllers/AIController.ts
+++ src/controllers/AIController.ts
@@ -1,8 +1,9 @@
 import { AuthenticatedRequest } from '../middlewares/authMiddleware';
 import { Response } from 'express';
 import dotenv from 'dotenv';
-import axios from 'axios';
 import { AIRepository } from '../repositories/AIRepository';
+import * as CryptoJS from 'crypto-js';
+import redisClient from '../redis/config';
 
 dotenv.config();
 
@@ -59,8 +60,21 @@ export class AIController {
                 answer: answers[index],
             }));
 
+            const cacheKey = this.generateCacheKey(questionAnswerPairs);
+
+            const cachedResponse = await redisClient.get(cacheKey);
+            if (cachedResponse) {
+                console.log('Cache hit for key:', cacheKey);
+                res.status(200).json({
+                    response: JSON.parse(cachedResponse),
+                });
+                return;
+            }
+
             let rawResponse =
                 await this.aiRepository.handleBatchGenerateDistractors(questionAnswerPairs);
+
+            await redisClient.set(cacheKey, JSON.stringify(rawResponse), 'EX', 3600 * 24 * 7);
 
             res.status(200).json({
                 response: rawResponse,
@@ -69,5 +83,11 @@ export class AIController {
             console.log('Error getting distractors from ai server: ' + error);
             res.status(500).send('Internal Server Error');
         }
+    };
+
+    private generateCacheKey = (data: any): string => {
+        const inputData = JSON.stringify(data);
+        const hash = CryptoJS.MD5(inputData).toString();
+        return `distractors:${hash}`;
     };
 }
